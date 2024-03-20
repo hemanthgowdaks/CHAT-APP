a023cddd5e0f4036927a83aaa80315eb5b6dd6cc
Hemanth <hemanthks2000@gmail.com>
2024-03-20 23:39:11 +0530

    initial commit with basic backend middlewares

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", (req,res)=>{
+    res.send("login route")
+})
+
+router.get("/signup", (req,res)=>{
+    res.send("signup route")
+})
+
+router.get("/signin", (req,res)=>{
+    res.send("signin route")
+})
+
+export default router;
