99de4ace82d9e6407c73f64c6612a7184aecf3e1
Hemanth <hemanthks2000@gmail.com>
2024-03-23 22:48:12 +0530

    create signup backend mark 1

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,6 +2,18 @@ export const signup = async (req,res) => {
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
 
+        if(password !== confirmPassword){
+            res.status(400).json({error:"Password don't match"})
+        }
+
+        const user = User.findOne({username})
+        
+        if(user){
+            res.status(400).json({error:"username already exists"})
+        }
+
+        //HASH password here
+
         
 
     } catch(error){
