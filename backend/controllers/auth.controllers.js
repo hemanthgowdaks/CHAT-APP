f44bc5f1074aa5ac01f62948b7ba2a8385c38bb1
Hemanth <hemanthks2000@gmail.com>
2024-03-24 16:47:33 +0530

    signup backend along with jwt and cookie

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,8 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateTokens.js"
+
+
 export const signup = async (req,res) => {
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
@@ -6,17 +11,49 @@ export const signup = async (req,res) => {
             res.status(400).json({error:"Password don't match"})
         }
 
-        const user = User.findOne({username})
+        const user = await User.findOne({username})
         
         if(user){
             res.status(400).json({error:"username already exists"})
         }
 
         //HASH password here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        
 
+        //for profile pic
+        // https://avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            confirmPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        })
+
+        if(newUser){
+            //generate jwt here
+            generateTokenAndSetCookie(newUser._id, res)
+
+
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                username:newUser.username,
+                profilePic:newUser.profilePic,
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
     } catch(error){
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
 
     }
     
---
+++ backend/utils/generateTokens.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+
+const generateTokenAndSetCookie = (userID, res) => {
+    const token = jwt.sign({userID}, process.env.JWT_SECRET,{
+        expiresIn: "15d"
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 15*24*60*60*1000,//15day is expressed in milliseconds days*24hours*60min*60sec*1000millisecondsInOneSecond
+        httpOnly: true,// prevent XSS attacks cross-site scripting attacks
+        sameSite: "strict", //CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
+    })
+
+}
+export default generateTokenAndSetCookie;
