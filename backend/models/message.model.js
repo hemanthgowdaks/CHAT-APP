9d255a2eafd48bc94a169c3dfa18c6f20c054b2f
Hemanth <hemanthks2000@gmail.com>
2024-03-25 22:21:47 +0530

    create backend to send message and store it in the database

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -7,7 +7,7 @@ const messageSchema = new mongoose.Schema(
             ref:"User",
             required:true,
         },
-        receiverrId: {
+        receiverId: {
             type:mongoose.Schema.Types.ObjectId,
             ref:"User",
             required:true,
